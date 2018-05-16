e42a50869ec3a2765dd5c383682cdd2011e7ef61
marian-daogaru <marian@phyramid.com>
2018-05-16 17:59:16 +0300

    l118

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,12 +6,21 @@ import BurgerIngredient from './BurgerIngredients/BurgerIngredient'
 
 const burger = (props) => {
 
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
       return [...Array(props.ingredients[igKey])].map((_, index) => {
         return <BurgerIngredient key={igKey + index} type={igKey} />
       })
     })
+    .reduce((arr, el) => {
+      return arr.concat(el)
+    }, [])
+
+  console.log(transformedIngredients)
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>!!!Please add ingredients!!!</p>
+  }
 
   return (
     <div className={classes.Burger}>
