fdd70fe96d8b7861258d854eaa84d48e0cc26aa8
marian-daogaru <marian@phyramid.com>
2018-05-17 14:04:01 +0300

    l125

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import Aux from '../../../hoc/Aux'
+
+const orderSummary = (props) => {
+  const ingredientSummary = Object.keys(props.ingredients)
+    .map(igKey => {
+      return (
+        <li key={igKey}>
+          <span style={{textTransform: 'capitalize'}}>{igKey}:</span> {props.ingredients[igKey]}
+        </li>
+      )
+    })
+
+
+  return (
+    <Aux>
+      <h3>Your order</h3>
+      <p>Top quality burger with the follsing ingredients:</p>
+      <ul>
+        {ingredientSummary}
+      </ul>
+      <p>Continue to checkout?</p>
+    </Aux>
+  )
+}
+
+export default orderSummary
