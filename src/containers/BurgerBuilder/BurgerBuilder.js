c300320c9e8d43d640f84845c4b813db4b7e9603
marian-daogaru <marian@phyramid.com>
2018-05-17 13:03:45 +0300

    l124

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -23,7 +23,22 @@ class BurgerBuilder extends Component {
         meat: 0,
       },
       totalPrice: 4,
+      purchasable: false,
     }
+  }
+
+  updatePurchaseState = () => {
+    const ingredients = {
+      ...this.state.ingredients
+    }
+    const sum = Object.keys(ingredients)
+      .map(igKey => {
+        return ingredients[igKey]
+      })
+      .reduce((sum, el) => {
+        return sum + el
+      }, 0)
+    this.setState({purchasable: sum > 0})
   }
 
   addIngredientHandler = (type) => {
@@ -41,7 +56,7 @@ class BurgerBuilder extends Component {
     this.setState({
       ingredients: updatedIngredients,
       totalPrice: newPrice
-    })
+    }, this.updatePurchaseState)
   }
 
   removeIngredientHandler = (type) => {
@@ -62,7 +77,7 @@ class BurgerBuilder extends Component {
     this.setState({
       ingredients: updatedIngredients,
       totalPrice: newPrice
-    })
+    }, this.updatePurchaseState)
 
   }
 
@@ -85,6 +100,7 @@ class BurgerBuilder extends Component {
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
           price={this.state.totalPrice}
+          purchasable={this.state.purchasable}
         />
       </Aux>
     )
