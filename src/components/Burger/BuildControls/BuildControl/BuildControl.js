e19cccd4c05ab53b4955bb7d54073040fa7d8d68
marian-daogaru <marian@phyramid.com>
2018-05-17 12:40:38 +0300

    l122

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -7,7 +7,13 @@ const buildControl = (props) => (
     <div className={classes.Label}>
       {props.label}
     </div>
-    <button className={classes.Less}>Less</button>
+    <button
+      className={classes.Less}
+      onClick={props.removed}
+      disabled={props.disabled}
+    >
+      Less
+    </button>
     <button
       className={classes.More}
       onClick={props.added}
