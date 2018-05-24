ab2ff7513af3df12840b739bf29b198d0b4268d7
marian-daogaru <marian@phyramid.com>
2018-05-24 17:32:03 +0300

    l212

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
-
+import {Route} from 'react-router-dom'
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
-
+import ContactData from './ContactData/ContactData'
 
 class Checkout extends Component {
 
@@ -39,6 +39,7 @@ class Checkout extends Component {
           onCheckoutCancelled={this.onCheckoutCancelled}
           onCheckoutContinued={this.onCheckoutContinued}
         />
+        <Route path={this.props.match.path +'/contact-data'} component={ContactData} />
       </div>
     )
   }
